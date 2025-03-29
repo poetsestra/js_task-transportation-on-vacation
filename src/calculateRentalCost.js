b777bdcdabe46e5a2ef64b7aae923a26e9314bbe
Zdena Bobosh <poetsestra@gmail.com>
2025-03-29 13:28:20 +0200

    Improve code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,12 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let totalSum = 40 * days;
+  const DAILY_RATE = 40;
+  const MIN_RENT_DAYS_MID_TERM_DISCOUNT = 3;
+  const MIN_RENT_DAYS_LONG_TERM_DISCOUNT = 7;
+  const MID_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
 
-  if (days >= 7) {
-    totalSum = totalSum - 50;
-  } else if (days >= 3) {
-    totalSum = totalSum - 20;
+  const totalSum = DAILY_RATE * days;
+
+  if (days >= MIN_RENT_DAYS_LONG_TERM_DISCOUNT) {
+    return totalSum - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MIN_RENT_DAYS_MID_TERM_DISCOUNT) {
+    return totalSum - MID_TERM_DISCOUNT;
   }
 
   return totalSum;
